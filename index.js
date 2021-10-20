8e5ce7420cf9e8f3c34a9b96d264f6113e5557e9
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-10-20 16:31:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 "use strict";
+
+module.exports = {
+
+    client: {
+
+    },
+
+    rest: {
+
+    },
+
+    src: {
+
+        foundation: {
+
+        },
+
+        util: {
+            BitField: require("./src/util/BitField"),
+            Color: require("./src/util/Color"),
+            Util: require("./src/util/Util")
+        }
+
+    }
+
+};
