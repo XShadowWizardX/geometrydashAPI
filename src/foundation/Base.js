a631aef09540f8ba7b2879ff106b94c2f76d7fcf
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-10-20 21:07:11 -0500

    Update Base.js

--- src/foundation/Base.js
+++ src/foundation/Base.js
@@ -8,31 +8,6 @@ const Util = require("../util/Util");
 
 class Base {
 
-    constructor(data) {
-        this.build();
-        this.parse(data);
-        this.patch();
-    }
-
-    build() {
-        
-        this._indexes = {}
-
-        return this;
-
-    }
-
-    patch() { return this; }
-
-    parse() { return this; }
-
-    /**
-     * @description Converts the instance into a string format
-     * @returns {string}
-     */
-
-    stringify() { return ""; }
-
     /**
      * @description Creates and returns a flattened instance converted
      * into a JSON style object
@@ -42,7 +17,5 @@ class Base {
     JSON() { return Util.flatten(this); }
 
 }
-
-Base.PROPERTIES = {};
 
 module.exports = Base;
