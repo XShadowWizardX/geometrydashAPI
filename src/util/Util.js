c7d61f9d6f3926f59db2fe16ef71b9aab318cded
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-10-21 10:03:35 -0500

    Update Util.js (groupping)

--- src/util/Util.js
+++ src/util/Util.js
@@ -5,6 +5,54 @@ const BitField = require("./BitField");
 class Util {}
 
 /**
+ * @description Takes a list of entries and divides them into array groups
+ * @param {number|number[]} n The number of entries per group
+ * @param {...*} entries The entries to divide into groups
+ * @returns {*[][]}
+ */
+
+Util.group = function(n, ...entries) { 
+    if (typeof n === "number") n = [ n ];
+    return entries.reduce((res, a) => {
+        let row = res.data[res.dataIndex] || [];
+
+        if (row.length >= res.nValue) {
+            if (res.nIndex + 1 < n.length) {
+                res.nIndex++;
+                res.nValue = n[res.nIndex];
+            }
+            res.dataIndex++;
+        }
+        
+        row = res.data[res.dataIndex] || [];
+        res.data[res.dataIndex] = [...row, a];
+
+        return res;
+    }, {
+        nIndex: 0,
+        nValue: n[0],
+        dataIndex: 0,
+        data: []
+    }).data;
+}
+
+/**
+ * @description Encodes a each character in a string to another character
+ * @param {string} str The string to encode 
+ * @param {number} [key=0] The exponent to encode with
+ * @returns {string}
+ */
+
+Util.xor = function(str, key=0) { 
+    if (typeof str !== "string")
+        throw new Error("\"str\" must be a string");    
+    return str.split("").map(c => {
+        c = c.charCodeAt();
+        return String.fromCodePoint(c ^ key);
+    }).join("");
+}
+
+/**
  * @param {*} data 
  * @returns {boolean} Whether the entered data is a normal object
  */
