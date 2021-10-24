8b6cd321586b43bd371403ffcde525e4532b84f6
XShadowWizardX <68756969+XShadowWizardX@users.noreply.github.com>
2021-10-24 09:46:08 -0500

    ADDED: properties for UserInfo and MapPack

---
+++ src/properties/levels/MapPack.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = {
+    PACK_ID: 1n,
+    NAME: 2n,
+    LEVELS: 3n,
+    STARS: 4n,
+    SCOINS: 5n,
+    DIFFICULTY_FACE: 6n,
+    NAME_COLOR: 7n,
+    PROGRESS_COLOR: 8n
+};
